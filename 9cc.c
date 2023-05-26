be4179d760ef10e2e6735a01fc0cb155be8ad660
yakishamo <82935423+yakishamo@users.noreply.github.com>
2023-05-26 14:00:01 +0900

    カッコと乗除算を実装

--- 9cc.c
+++ 9cc.c
@@ -5,28 +5,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-// トークンの種類
-typedef enum {
-	TK_RESERVED, //記号
-	TK_NUM,      //整数トークン
-	TK_EOF,      //入力の終わりを表すトークン
-} TokenKind;
-
-typedef struct Token Token;
-
-//トークン型
-struct Token {
-	TokenKind kind; //トークンの型
-	Token *next;    //次の入力トークン
-	int val;        //kindがTK_NUMの場合、その数値
-	char *str;      //トークン文字列
-};
+#include "9cc.h"
 
 //現在着目しているトークン
 Token *token;
 
 //入力プログラム
 char *user_input;
+
+Node *expr();
+Node *mul();
+Node *primary();
 
 //エラーを報告するための関数
 //printfと同じ引数をとる
@@ -107,7 +96,8 @@ Token *tokenize() {
 			continue;
 		}
 
-		if(*p == '+' || *p == '-') {
+		if(*p == '+' || *p == '-' || *p == '*' || *p == '/' ||
+				*p == '(' || *p == ')') {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -118,11 +108,94 @@ Token *tokenize() {
 			continue;
 		} 
 		
-		error(token->str, "invalid token");
+		error("invalid token");
 	}
 
 	new_token(TK_EOF, cur, p);
 	return head.next;
+}
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val) {
+	Node *node = calloc(1,sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *expr() {
+	Node *node = mul();
+	for(;;) {
+		if(consume('+')) {
+			node = new_node(ND_ADD, node, mul());
+		} else if(consume('-')) {
+			node=  new_node(ND_SUB, node, mul());
+		} else {
+			return node;
+		}
+	}
+}
+
+Node *mul() {
+	Node *node = primary();
+	for(;;) {
+		if(consume('*')) {
+			node = new_node(ND_MUL, node, primary());
+		} else if(consume('/')) {
+			node = new_node(ND_DIV, node, primary());
+		} else {
+			return node;
+		}
+	}
+}
+
+Node *primary() {
+	//次のトークンが"("なら"(" expr ")"のはず
+	if(consume('(')) {
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+
+	//そうでなければ数値のはず
+	return new_node_num(expect_number());
+}
+
+void gen(Node *node) {
+	if(node->kind == ND_NUM) {
+		printf("	push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("	pop rdi\n");
+	printf("	pop rax\n");
+
+	switch(node->kind) {
+		case ND_ADD:
+			printf("	add rax, rdi\n");
+			break;
+		case ND_SUB:
+			printf("	sub rax, rdi\n");
+			break;
+		case ND_MUL:
+			printf("	imul rax, rdi\n");
+			break;
+		case ND_DIV:
+			printf("	cqo\n");
+			printf("	idiv rdi\n");
+			break;
+	}
+	printf("	push rax\n");
 }
 
 int main(int argc, char **argv) {
@@ -133,23 +206,16 @@ int main(int argc, char **argv) {
 	
 	user_input = argv[1];
 	token = tokenize();
-	
+
+	Node *top = expr();
+
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
-
-	printf("	mov rax, %d\n", expect_number());
-
-	while(!at_eof()) {
-		if(consume('+')) {
-			printf("	add rax, %d\n", expect_number());
-			continue;
-		}
-
-		expect('-');
-		printf("	sub rax, %d\n", expect_number());
-	}
-
+	
+	gen(top);
+	
+	printf("	pop rax\n");
 	printf("	ret\n");
 	return 0;
 }
